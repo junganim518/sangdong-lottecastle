c0c6dc5f662ceae08e004846614b59dd093a180e
junganim518 <junganim518@gmail.com>
2026-07-24 17:32:45 +0900

    SEO: robots.txt의 sitemap URL을 www 버전으로 변경

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://sangdonglottecastle.co.kr/sitemap.xml",
+    sitemap: "https://www.sangdonglottecastle.co.kr/sitemap.xml",
   };
 }
